acbd9b9ab9a852fb22f81ccf15f7db7623d5d8bb
Gurkirat Singh <tbhaxor@gmail.com>
2025-06-15 02:55:37 +0530

    improve: ingore src/utils from coverage
    Specs for get-global file not being used. Renamed to spect

--- jest.config.js
+++ jest.config.js
@@ -1,7 +1,7 @@
 module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'node',
-  collectCoverageFrom: ['src/**/*.ts', '!src/**/index.ts', '!src/**/*.interface.ts'],
+  collectCoverageFrom: ['src/**/*.ts', '!src/utils/*.ts', '!src/**/index.ts', '!src/**/*.interface.ts'],
   globals: {
     'ts-jest': {
       tsconfig: 'tsconfig.spec.json',
